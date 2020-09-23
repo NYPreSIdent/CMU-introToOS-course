d0d56253d6bab6866f0c9b5019c116bc843d4c99
Zhiyang Wang <liasosong546263@gmail.com>
2020-09-23 21:40:44 +0800

    boomlab plus

--- TheCProgramming/lab01/hello.c
+++ TheCProgramming/lab01/hello.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    int i;
-    int count = 0;
-    int *p = &count;
+    unsigned i;
+    unsigned a = 20;
+    unsigned b = 21;
+    i = a - b;
+    printf("%u", i);    
 
-    for (i = 0; i < 10; i++) {
-        (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
-    }
-
-    printf("Thanks for waddling through this program. Have a nice day.");
-    return 0;
 }
